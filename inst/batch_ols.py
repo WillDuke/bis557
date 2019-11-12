898fa5b981a1057845e02dc71ffb9e64de59dd28
Will Duke <willduke.wd@gmail.com>
2019-11-12 12:10:01 -0500

    refactor batch_ols and remove divisibility req

--- inst/batch_ols.py
+++ inst/batch_ols.py
@@ -1,49 +1,61 @@
+import numpy as np
 import csv
 
-def batch_ols(file, chunksize = 10, ycol = 0):
+def crossprods(yX_subset, chunksize, ycol, row):
+    # convert to array
+    chunk = np.array(yX_subset.reshape(-1, len(row)))
+    # separate y and X
+    y_chunk = chunk[:, ycol]
+    X_chunk = np.array(np.delete(chunk, ycol, axis=1))
+
+    # add intercept
+    intercept = np.repeat(1,y_chunk.shape).reshape(-1,1)
+    X_chunk = np.hstack((intercept, X_chunk))
+
+    # compute xtx and xty for chunk
+    xtx = X_chunk.T @ X_chunk
+    xty = (X_chunk.T @ y_chunk).reshape(-1,1)
+
+    return xtx, xty
+
+def batch_ols(file, chunksize = 15, ycol = 0):
 
     with open('yX_ols.csv', 'r') as csvfile:
         csvreader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC)
-        yX_subset = []
+        yX_subset, whole_xtx, whole_xty = [],[],[]
         i = 0
-        j = 0
         for row in csvreader:
             yX_subset = np.append(yX_subset, row)
             i += 1
-            
-            if i == chunksize :
-                # create np array of subset
-                yXarr = np.array(yX_subset.reshape(-1, len(row)))
 
-                # separate y and X
-                y_chunk = yXarr[:,ycol]
-                X_chunk = np.array(np.delete(yXarr, ycol, axis=1))
+            if i % chunksize == 0:
+                # find xtx and xty
+                xtx, xty = crossprods(yX_subset, chunksize, ycol, row)
 
-                # add intercept
-                intercept = np.repeat(1,i).reshape(-1,1)
-                X_chunk = np.hstack((intercept, X_chunk))
-
-                # compute xtx and xty for chunk
-                xtx = X_chunk.T @ X_chunk
-                xty = (X_chunk.T @ y_chunk).reshape(-1,1)
-
-                # add the new xtx
-                if j == 0:
+                #iteratively sum whole xtx and xty arrays
+                if len(whole_xtx) == 0:
                     whole_xtx = xtx.copy()
-
-                else: whole_xtx = np.add(whole_xtx, xtx)
-
-                if j == 0:
+                else: whole_xtx = np.add(whole_xtx, xtx.copy())
+                if len(whole_xty) == 0:
                     whole_xty = xty.copy()
+                else: whole_xty = np.add(whole_xty, xty.copy())
 
-                else: whole_xty = np.add(whole_xty, xty)
-
-                # reset subset and row counter, increment chunk counter
+                # reset subset
                 yX_subset = []
-                i = 0
-                j += 1
-                
+
+
+        # add last matrices if csv.shape[0] % chunksize != 0        
+        if len(yX_subset) != 0:
+            xtx, xty = crossprods(yX_subset, chunksize, ycol, row)
+            whole_xtx = np.add(whole_xtx, xtx.copy())
+            whole_xty = np.add(whole_xty, xty.copy())
+
     coef = np.linalg.solve(whole_xtx, whole_xty)
-    return coef
-            
-batch_ols("yX.csv")
+
+    keys = ["V" + str(num) for num in range(0, coef.shape[0])]
+    nonZeroBetas = {x:y for x,y in dict(zip(keys, coef)).items() if y != 0}
+    
+    for key in nonZeroBetas.keys():
+        nonZeroBetas[key] = round(float(nonZeroBetas[key]),4)
+
+    return nonZeroBetas
